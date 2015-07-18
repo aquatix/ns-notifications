56bae8acf69d411840c0269e0a1264717ea3df36
Michiel Scholten <michiel@diginaut.net>
2015-07-18 21:19:07 +0200

    Reorganisation, trips

--- ns_notifications.py
+++ ns_notifications.py
@@ -6,17 +6,21 @@ from pushbullet import PushBullet
 import pushbullet
 #import pylibmc
 from pymemcache.client import Client as MemcacheClient
+import datetime
 #import simplejson as json
 import json
-import __main__ as main
 import requests
+import __main__ as main
 import sys
 
 import settings
 
-#mc = pylibmc.Client(['127.0.0.1'], binary=True, behaviors={'tcp_nodelay': True, 'ketama': True})
+# Only plan routes that are at maximum half an hour in the past or an hour in the future
+MAX_TIME_PAST = 1800
+MAX_TIME_FUTURE = 3600
 
 
+## Helper functions for memcache serialisation
 def json_serializer(key, value):
     if type(value) == str:
         return value, 1
@@ -33,6 +37,7 @@ def json_deserializer(key, value, flags):
     raise Exception("Unknown serialization format")
 
 
+## Open memcache
 mc = MemcacheClient(('127.0.0.1', 11211), serializer=json_serializer,
         deserializer=json_deserializer)
 
@@ -63,13 +68,28 @@ if __name__ == '__main__':
     errors = []
     nsapi = ns_api.NSAPI(settings.username, settings.apikey)
 
-    #with open('storingen.xml') as fd:
-    #    disruptions = nsapi.parse_disruptions(fd.read())
+    ## Get the list of stations
+    try:
+        stations = mc.get('stations')
+    except KeyError:
+        stations = []
+        try:
+            stations = nsapi.get_stations()
+        except requests.exceptions.ConnectionError:
+            print('Something went wrong connecting to the API')
+
+        stations_json = ns_api.list_to_json(stations)
+        # Cache the stations
+        mc.set('stations', stations_json)
+
+
+    ## Get the current disruptions (globally)
     try:
         disruptions = nsapi.get_disruptions()
 
         prev_disruptions = mc.get('prev_disruptions')
-        if prev_disruptions == None:
+        # TODO: check whether this went ok
+        if prev_disruptions == None or prev_disruptions == []:
             prev_disruptions = {'unplanned': [], 'planned': []}
 
         prev_disruptions['unplanned'] = ns_api.list_from_json(prev_disruptions['unplanned'])
@@ -82,11 +102,9 @@ if __name__ == '__main__':
 
         prev_unplanned = new_or_changed_unplanned + unchanged_unplanned
 
-
-
         #new_or_changed_planned = ns_api.list_diff(prev_disruptions['planned'], disruptions['planned'])
         #print(new_or_changed_planned)
-        #for plan in new_or_changed_planned: 
+        #for plan in new_or_changed_planned:
         #    print plan.key
         #    print plan.message
         #    print "------"
@@ -96,37 +114,41 @@ if __name__ == '__main__':
         #prev_planned = new_or_changed_planned + unchanged_planned
 
         # Update the cached list with the current information
-        #prev_unplanned_json = ns_api.list_to_json(prev_unplanned)
-        #mc.set('prev_disruptions', ns_api.list_to_json(prev_unplanned))
+        mc.set('prev_disruptions', {'unplanned': ns_api.list_to_json(prev_unplanned), 'planned': []})
 
     except requests.exceptions.ConnectionError as e:
         print('[ERROR] connectionerror doing disruptions')
         errors.append(('Exception doing disruptions', e))
 
-    try:
-        #stations = mc['stations']
-        stations = mc.get('stations')
-    except KeyError:
-        stations = []
-        try:
-            stations = nsapi.get_stations()
-        except requests.exceptions.ConnectionError:
-            print('Something went wrong connecting to the API')
 
-        stations_json = ns_api.list_to_json(stations)
-        # Cache the stations
-        #mc['stations'] = stations_json
-        mc.set('stations', stations_json)
-
+    ## Get the information on the list of trips configured by the user
     try:
+        today = datetime.datetime.now().strftime('%d-%m')
+        today_date = datetime.datetime.now().strftime('%d-%m-%Y')
+        current_time = datetime.datetime.now()
+
         trips = []
 
         #for route in settings.routes:
-        #    print route['time'] + ' ' + route['departure']
+        #    if current_time > route_time and delta.total_seconds() > MAX_TIME_PAST:
+        #        # the route was too long ago ago, lets skip it
+        #        logger.info('route %s was too long ago, skipped', route)
+        #        continue
+        #    if current_time < route_time and abs(delta.total_seconds()) > MAX_TIME_FUTURE:
+        #        # the route is too much in the future, lets skip it
+        #        logger.info('route %s was too much in the future, skipped', route)
+        #        continue
+        #        print route['time'] + ' ' + route['departure']
         #    trips = nsapi.get_trips(route['time'], route['departure'], route['keyword'], route['destination'])
 
         route = settings.routes[0]
+        route_time = datetime.datetime.strptime(today_date + " " + route['time'], "%d-%m-%Y %H:%M")
+        delta = current_time - route_time
         print route['time'] + ' ' + route['departure']
+        if current_time > route_time and delta.total_seconds() > MAX_TIME_PAST:
+            print "TOO LATE"
+        if current_time < route_time and abs(delta.total_seconds()) > MAX_TIME_FUTURE:
+            print "TOO EARLY"
         trips = nsapi.get_trips(route['time'], route['departure'], route['keyword'], route['destination'], True)
         optimal_trip = ns_api.Trip.get_optimal(trips, route['time'])
 
@@ -140,6 +162,8 @@ if __name__ == '__main__':
         print('[ERROR] connectionerror doing trips')
         errors.append(('Exception doing trips', e))
 
+
+    ## Wrapping up
     print "errors:"
     print errors
 
