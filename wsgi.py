60007ddd8d62f42c9bd5b45394b86d4d34e3baed
Michiel Scholten <michiel@diginaut.net>
2018-03-08 13:40:19 +0100

    Fixed Python 3 compatibility

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # Activate virtualenv
+import sys
 import settings
-activate_this = settings.VENV
-execfile(activate_this, dict(__file__=activate_this))
+activate_this = getattr(settings, 'VENV', None)
+
+if (sys.version_info > (3, 0)):
+    # Python 3
+    with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
+else:
+    # Python 2
+    if activate_this:
+        execfile(activate_this, dict(__file__=activate_this))
 
 from server import app as application
 
